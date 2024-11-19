0a3761576d1b1519e7c23de3fcd853127bef2451
Saratsin-25 <7422037@gmail.com>
2024-11-19 15:45:24 +0300

    Создано окно и виджеты

--- cataas.py
+++ cataas.py
@@ -1,2 +1,24 @@
+from cProfile import label
 from tkinter import *
 import requests
+from PIL import Image, ImageTk
+from io import BytesIO
+
+from pygame.examples.aliens import load_image
+
+window = Tk()
+window.title("Cats")
+window.geometry("600x480")
+
+label = Label()
+label.pack()
+
+url="https://cataas.com/cat"
+img=load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
+
