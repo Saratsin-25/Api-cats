355d35d1eee53331431c585acede4b68718a3dc6
Saratsin-25 <7422037@gmail.com>
2024-11-19 15:56:08 +0300

    Создана функция для получения изображения котиков

--- cataas.py
+++ cataas.py
@@ -1,8 +1,19 @@
-from cProfile import label
 from tkinter import *
 import requests
 from PIL import Image, ImageTk
 from io import BytesIO
+
+
+def load_image():
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"Ошибка {e}")
+        return None
 
 from pygame.examples.aliens import load_image
 
