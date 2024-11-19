cad8a5b923271f17c85ae27bb9a424409ca99507
Saratsin-25 <7422037@gmail.com>
2024-11-19 17:03:35 +0300

    Добавлено получение изображений по тегу и без оного

--- cataas.py
+++ cataas.py
@@ -18,7 +18,9 @@ def load_image(url):
 
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
+    img = load_image(url_tag)
 
     if img:
         new_window = Toplevel()
@@ -33,16 +35,21 @@ window = Tk()
 window.title("Cats")
 window.geometry("600x520")
 
+tag_entry = Entry()
+tag_entry.pack()
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
+
+
 menu_bar=Menu(window)
 window.config(menu=menu_bar)
 
 url="https://cataas.com/cat"
 
-set_image()
-
 file_menu=Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Файл", menu=file_menu)
-file_menu.add_command(label="Обновить", command=open_new_window)
+file_menu.add_command(label="Загрузить фото", command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command=window.destroy)
 
